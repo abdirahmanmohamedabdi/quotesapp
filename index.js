e4de58b08b51dce653f61a26c5bc8c420eb34978
AbdirahmanAbdi <106161034+abdirhamanabdi@users.noreply.github.com>
2022-06-24 13:33:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b927733739mshbab0909e1347b4ep1c1a2fjsn652f8f4dff61',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+    .then(data => 
+
+        {
+
+const name = document.getElementById("Author").innerHTML = data.originator.name
+const quote = document.getElementById("quote").innerHTML = data.content
+const wordInput = document.getElementById("review");
+    
+        })
+
+       
+          
+          function refreshPage(){
+            window.location.reload();
+            push.preventDefault();
+        } 
+
+        
